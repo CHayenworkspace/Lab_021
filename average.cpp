185b339409fc0bc725aae011668cfde381b8301b
chayen030 <chayenlol268@gmail.com>
2025-02-18 15:16:38 +0700

    test102

--- average.cpp
+++ average.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 int main (int argc, char* argv[]){
     if(argc==1){
-        cout<< "Please input numbers to find average.";
-    }
-    int i = 1;
+        cout<< "Please input numbers to find average.\n";
+    }else{
+    int i = 0;
     int count = 0;
+    int y = argc;
     double x = 0;
-    if(i>=argc){
-        double num = atoi(argv[i]);
+    if(i <= y){
+        double num = atoi(argv[i+1]);
         x = x + num;
         count++;
     }
+    cout << "---------------------------------\n";
+    cout <<"Average of "<< count <<" numbers = "<< x/count << "\n";
     cout << "---------------------------------";
-    cout <<"Average of "<< count <<" numbers = "<< x/count;
-    cout << "---------------------------------";
-    return 0;
+    return 0;}
 }
