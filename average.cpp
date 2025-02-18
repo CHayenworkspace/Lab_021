085762058533ea9e6b97033bc36a59426c1bfc9f
chayen030 <chayenlol268@gmail.com>
2025-02-18 15:12:39 +0700

    test101

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main (int argc, char* argv[]){
+    if(argc==1){
+        cout<< "Please input numbers to find average.";
+    }
+    int i = 1;
+    int count = 0;
+    double x = 0;
+    if(i>=argc){
+        double num = atoi(argv[i]);
+        x = x + num;
+        count++;
+    }
+    cout << "---------------------------------";
+    cout <<"Average of "<< count <<" numbers = "<< x/count;
+    cout << "---------------------------------";
+    return 0;
+}
